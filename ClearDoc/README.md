03ccd200860c6d788b2ff4151685be55be57298f
Mikhail Malakhov <malakhv@gmail.com>
2022-08-01 15:26:34 +0300

    Added old Delphi project - ClearDoc

---
+++ ClearDoc/README.md
@@ -0,0 +1,3 @@
+# ClearDoc
+
+Program to clear list of recent documents in Start panel (Windows).
---
+++ ClearDoc/clDoc.dpr
@@ -0,0 +1,9 @@
+program clDoc;
+{$APPTYPE CONSOLE}
+uses
+  ShlOBJ;
+
+begin
+  SHAddToRecentDocs(SHARD_PATH, nil);
+end.
+ 
